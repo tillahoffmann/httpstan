9bb27b88c6ade28edeec0bf941a460df6e76fd25
Till Hoffmann <tillahoffmann@gmail.com>
2021-11-11 12:23:27 +0000

    Build stan libraries with build_ext.

--- build.py
+++ build.py
@@ -1,4 +1,9 @@
 from distutils.extension import Extension
+from distutils.command.build_ext import build_ext
+import pathlib
+import shutil
+import subprocess
+
 
 # empty extension module so build machinery recognizes package as platform-specific
 extensions = [
@@ -14,5 +19,23 @@ extensions = [
 ]
 
 
+class BuildExtCommand(build_ext):
+    def run(self) -> None:
+        # Copy the makefiles and compile the libraries.
+        build_lib = pathlib.Path(self.build_lib)
+        filenames = ['Makefile', 'Makefile.libraries']
+        for filename in filenames:
+            shutil.copy(filename, build_lib / filename)
+        subprocess.check_call(['make'], cwd=self.build_lib)
+        super().run()
+        # Remove the `build` library so it doesn't get packaged.
+        shutil.rmtree(build_lib / 'build')
+
+
 def build(setup_kwargs):
-    setup_kwargs.update({"ext_modules": extensions})
+    setup_kwargs.update({
+        "ext_modules": extensions,
+        "cmdclass": {
+            "build_ext": BuildExtCommand,
+        },
+    })
